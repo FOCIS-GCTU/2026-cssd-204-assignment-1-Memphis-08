1660b27c0f5e23f9a61b732b87716453526d79bc
Memphis-08 <2425404023@live.gctu.edu.gh>
2026-05-30 21:14:14 +0845

    Add files via upload

--- initials.py
+++ initials.py
@@ -1,7 +1,32 @@
+#This is my header
+print("#File:initials.py")
+print("#Description: Displaying my initials")
+print("#Assignment Number:1")
+print("#")
+print("#Name:Dwirah Victor")
+print("#STUDENT ID:2425404023")
+print("#Email:victordwirah@gmail.com")
+print("#Grader:Augustus")
+print("#")
+print("# On my honor,Augustus Buckman, this programming assignment is my own work ")
+print("#and I have not provided this code to any student")
+
 
 def main():
-  pass #code goes here
-
+    print("")
+    print("...VDM")
+    print("")
+    print("...VVVV....VVVV........DDDDDDDD............MMMM....MMMM.....")
+    print("...VVVV....VVVV........DDDDDDDDDD..........MMMM....MMMM.....")
+    print("...VVVV....VVVV........DD.......DDD........MM.MM..MM.MM.....")
+    print("...VVVV....VVVV........DD........DD........MM.MM..MM.MM.....")
+    print("...VVVV....VVVV........DD........DD........MM.MM..MM.MM.....")
+    print("...VVVV....VVVV........DD........DD........MM..MMMM..MM.....")
+    print("...VVVV....VVVV........DD........DD........MM..MMMM..MM.....")
+    print("....VVV....VVV.........DD.......DDD........MM...MM...MM.....")
+    print(".....VV....VV.....**...DDDDDDDDDD.....**...MM...MM...MM...**")
+    print("......VVVVVV......**...DDDDDDDDD......**...MM...MM...MM...**")
+    print("")
 
 
 main()
